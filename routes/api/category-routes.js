2a5ac3ce52f5467b0b6f509aab158f73683a85e6
Tyler Davis <tdnc85@yahoo.com>
2021-07-21 18:42:24 -0400

    npm i, and created models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,41 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [Product]
+  }).then(dbRes => {
+    res.json(dbRes)
+  }).catch(err => {
+    res.json(err)
+  }) 
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(dbRes => {
+    res.json(dbRes)
+  }).catch(err => {
+    res.json(err)
+  }) 
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // be sure to include its associated Products .findOne
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // create a new category .create
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // update a category by its `id` value .update
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  // delete a category by its `id` value .destroy
 });
 
 module.exports = router;
